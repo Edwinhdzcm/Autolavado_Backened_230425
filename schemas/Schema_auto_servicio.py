225630d890d9d06d9500625c462d1b111c59e1ac
Edwinhdzcm <230425@utxicotepec.edu.mx>
2026-02-10 14:29:38 -0600

    Edwin
    avance de schemas

---
+++ schemas/Schema_auto_servicio.py
@@ -0,0 +1,15 @@
+# pylint disable=import-error 
+from config.db import Base
+
+
+class Estatus(Enum);
+Programado = "Programado"
+Proceso = "En proceso"
+Realizado = "Realizado"
+
+# pylint: disable=too-few-public-methods
+
+
+
+
+
