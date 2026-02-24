2f1a6e46ac8e51a8902cc45cc789845245b0080d
Edwinhdzcm <230425@utxicotepec.edu.mx>
2026-02-24 14:10:55 -0600

    Avance 2
    Se agregaron cambios

--- schemas/Schema_auto_servicio.py
+++ schemas/Schema_auto_servicio.py
@@ -1,10 +1,34 @@
-# pylint disable=import-error 
-from config.db import Base
+'''
+Docstring for schemas.schema_usuario_vehiculo_servicio
+'''
+from typing import Optional
+from datetime import datetime
+from pydantic import BaseModel
+from pydantic import ConfigDict
 
+class UsuarioVehiculoServicioBase(BaseModel):
+    '''Clase para modelar los campos de tabla usuario_vehiculo_servicio'''
+    vehiculo_Id: int
+    cajero_Id: int
+    operativo_Id: int
+    servicio_Id: int
+    fecha: str
+    hora: str
+    estatus: str
+    estado: bool
+    fecha_registro: datetime
+    fecha_actualizacion: datetime
+# pylint: disable=too-few-public-methods, unnecessary-pass
+class UsuarioVehiculoServicioCreate(UsuarioVehiculoServicioBase):
+    '''Clase para crear un usuario_vehiculo_servicio basado en la tabla usuario_vehiculo_servicio'''
+    pass
+class UsuarioVehiculoServicioUpdate(UsuarioVehiculoServicioBase):
+    '''Clase para actualizar un usuario_vehiculo_servicio basado en la tabla usuario_vehiculo_servicio'''
+    pass
 
-class Estatus(Enum);
-Programado = "Programado"
-Proceso = "En proceso"
-Realizado = "Realizado"
-
-# pylint: disable=too-few-public-methods
+class UsuarioVehiculoServicio(UsuarioVehiculoServicioBase):
+    '''Clase para realizar operaciones por ID en tabla usuario_vehiculo_servicio'''
+    Id: int
+    class Config:
+        '''Utilizar el orm para ejecutar las funcionalidades'''
+        model_config = ConfigDict(from_attributes=True)
